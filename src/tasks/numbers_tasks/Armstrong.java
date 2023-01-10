ca7860aa1103908c8b3684155d1e31fbb012fa55
Hilal2013 <hilalozkul@gmail.com>
2023-01-10 09:22:15 -0500

    added armstrong task

---
+++ src/tasks/numbers_tasks/Armstrong.java
@@ -0,0 +1,49 @@
+package tasks.numbers_tasks;
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = scan.nextInt();
+        checkArmstrong(number);
+        scan.close();
+
+        System.out.println(isArmStrongNumber(153));
+    }
+
+    public static void checkArmstrong(int number) {
+        //to convert number to String
+        //  String stringNumber = "" + number;
+        String  stringNumber=String.valueOf(number);
+        int sum = 0;
+        for (int i = 0; i < stringNumber.length(); i++) {
+            String charNumber=""+stringNumber.charAt(i);//convert char to string
+            sum += Math.pow(Integer.parseInt(charNumber), stringNumber.length());
+            //convert string to Integer
+
+        }
+        if (sum == number) {
+            System.out.println(number + " is an armstrong number");
+        } else {
+            System.out.println(number + " is not an armstrong number! Try again!");
+        }
+    }
+    public static boolean isArmStrongNumber(int num) {
+        int a = 0, b = 0, c = num;
+
+        while (num > 0) {
+            a = num % 10;//153%10=3 => first digit
+            num /= 10;
+            b = b + (a * a * a);
+        }
+
+        return (c == b) ? true : false;
+    }
+
+
+}
+ /*
+    Write a method that can check if a number is Armstrong number
+     */
