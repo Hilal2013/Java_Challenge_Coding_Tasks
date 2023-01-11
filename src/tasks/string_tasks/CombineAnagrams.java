77f6e3e52231aeeae734ece01d174d861aed97df
Hilal2013 <hilalozkul@gmail.com>
2023-01-11 07:48:32 -0500

    updated

--- src/tasks/string_tasks/CombineAnagrams.java
+++ src/tasks/string_tasks/CombineAnagrams.java
@@ -32,4 +32,29 @@ input  str={"eat", "tea", "tam", "ate", "mat", "bat"}
 output {{"bat"},{"mat","tam"},{"ate","eat","tea"}}
 
 task is to combine words that are anagrams between themselves into a separate array
+
+  String s ="";
+            for (String each : list) {
+                s += each +" ";
+            }
+            // System.out.println(s);
+            if (!list2.contains(s)){
+                list2.add(s);
+            }
+            list.clear();
+        }
+
+        ArrayList<ArrayList<String>> last = new ArrayList<>();
+
+        for (int i = 0; i < list2.size(); i++) {
+
+            String[] a = list2.get(i).split(" ");
+            ArrayList<String> al = new ArrayList<>(Arrays.asList(a));
+            last.add(al);
+
+        }
+
+        System.out.println(last);
+
+
  */
