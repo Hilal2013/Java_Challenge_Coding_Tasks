41d51157719b4363edf54d37bf7b38ab2d667bdb
Hilal2013 <hilalozkul@gmail.com>
2023-01-11 07:48:05 -0500

    updated

--- src/tasks/string_tasks/CombineAnagrams.java
+++ src/tasks/string_tasks/CombineAnagrams.java
@@ -6,27 +6,24 @@ public class CombineAnagrams {
     public static void main(String[] args) {
         //   List<String> list=new ArrayList<>(Arrays.asList("eat", "tea", "tam", "ate", "mat", "bat"));
         String[] arr = {"eat", "tea", "tam", "ate", "mat", "bat"};
-      //  ArrayList<String> list = new ArrayList<>();
-     //   ArrayList<ArrayList<String>> innerList = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<ArrayList<String>> innerList = new ArrayList<>();
 
 
         for (int i = 0; i < arr.length; i++) {
-        char[] each=   arr[i].toCharArray();
+            char[] each = arr[i].toCharArray();
             Arrays.sort(each);
             for (int j = 0; j < arr.length; j++) {
-                char[] element  =arr[j].toCharArray();
+                char[] element = arr[j].toCharArray();
                 Arrays.sort(element);
-                if (Arrays.equals(each, element) ){
-             //       list.add(arr[i]);
-               //     list.add(i,arr[j]);
-              //    innerList.add(i,list);
-                }else{
-                 //   list.add(arr[i]);
-                 //   innerList.add(i,list);
+                if (Arrays.equals(each, element) && !list.contains(arr[j])) {
+                    list.add(arr[j]);//[eat, tea, ate, tam, mat, bat]
+
                 }
             }
+            innerList.add(i, list);
         }
-   //     System.out.println(innerList);
+        System.out.println(innerList);
     }
 }
 /*
