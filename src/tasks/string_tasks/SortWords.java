a80d21f58520f491827d7cb40d9b579bd8cdbc47
Hilal2013 <hilalozkul@gmail.com>
2023-01-13 17:23:57 -0500

    added  SortWords task

---
+++ src/tasks/string_tasks/SortWords.java
@@ -0,0 +1,26 @@
+package tasks.string_tasks;
+
+public class SortWords {
+}
+    /*
+
+    Sort String words in ascending order by length, No special Characters allowed or Digits except for a period (.), and the period must be at the end of the sorted string. Only the very first character of the first word after sorted must be Uppercase and all other characters must be lowercase. If two String words have the same length word, then the first occurring word should come before the other string words of same length.
+
+Example 1:
+
+Input: Cats and hats.
+
+Output: And cats hats.
+
+Example 2 :
+
+Input: i Love to Game!
+
+Output: I to love game.
+
+Example 3:
+
+Input: Blizzard is a place I want 2 work @t!
+
+Output: A i t is want work place blizzard.
+     */
