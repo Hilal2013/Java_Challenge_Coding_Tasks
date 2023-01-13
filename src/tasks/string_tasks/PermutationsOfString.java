c61d88df34a9d19934b81dd844a35ab70d0661c0
Hilal2013 <hilalozkul@gmail.com>
2023-01-13 11:09:43 -0500

    added PermutationsOfString task

---
+++ src/tasks/string_tasks/PermutationsOfString.java
@@ -0,0 +1,13 @@
+package tasks.string_tasks;
+
+public class PermutationsOfString {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+/*
+Input: S = “ABC”
+Output: “ABC”, “ACB”, “BAC”, “BCA”, “CBA”, “CAB”
+ */
