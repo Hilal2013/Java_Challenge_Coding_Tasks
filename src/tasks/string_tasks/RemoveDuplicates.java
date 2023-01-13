94d58882506322ea62c6ffeedd85a8e25263f9a6
Hilal2013 <hilalozkul@gmail.com>
2023-01-13 16:04:35 -0500

    updated and  task

--- src/tasks/string_tasks/RemoveDuplicates.java
+++ src/tasks/string_tasks/RemoveDuplicates.java
@@ -2,6 +2,26 @@ package tasks.string_tasks;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-
+        System.out.println(removeDup("AAABBBCCCD"));//ABCD
     }
+
+    public static String removeDup(String str){
+        String result="";
+        for (int i = 0; i <str.length() ; i++) {
+            char each=str.charAt(i);
+            if(!result.contains(each+"")){
+                result+=each;
+            }else{
+                continue;
+            }
+        }
+
+      return  result;
+    }
+
+
 }
+   /*
+    Write function that can remove the duplicated values from String
+            Ex: removeDup("AAABBBCCC") ==> ABC
+     */
