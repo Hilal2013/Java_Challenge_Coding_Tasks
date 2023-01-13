683c1c651eb0f16f75a1cae7ef56c33eb9b2e15f
Hilal2013 <hilalozkul@gmail.com>
2023-01-13 16:31:21 -0500

    added ReverseConsecutive Strings task

---
+++ src/tasks/string_tasks/ReverseConsecutiveStrings.java
@@ -0,0 +1,19 @@
+package tasks.string_tasks;
+
+public class ReverseConsecutiveStrings {
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+ /*
+    Write a program that can reverse  the consecutive strings.
+            Do not use any lambda or stream or any build in reverse methods
+        Ex:
+            input:
+                str = "test12pop90java989pyt"
+            output:
+                tset12pop90avaj989typ
+     */
