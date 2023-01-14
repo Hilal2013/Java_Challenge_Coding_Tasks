f7df81987cd0de9a8eae7c055c6bd802e59526eb
Hilal2013 <hilalozkul@gmail.com>
2023-01-13 22:03:38 -0500

    added  Happy Number task

---
+++ src/tasks/numbers_tasks/HappyAndUnhappyNumber.java
@@ -0,0 +1,30 @@
+package tasks.numbers_tasks;
+
+public class HappyAndUnhappyNumber {
+    public static void main(String[] args) {
+        checkHappyOrUnhappy(23);
+    }
+
+    public static void checkHappyOrUnhappy(int number){
+
+
+
+
+    }
+
+
+}
+/*
+
+write a function that can identify if a number is a happy Number or unhappy number
+
+23 is a happy number. Let’s see how.
+
+        23 = 22 + 32 = 4 + 9 = 13 (sum of the squares of its digits)
+
+        13 = 12 + 32 = 1 + 9 = 10 (sum of the squares of the digits)
+
+        10 = 12 + 02 = 1 (sum of the squares of the digits)
+        1, 13, 68,100,496,820,1000
+ )
+ */
