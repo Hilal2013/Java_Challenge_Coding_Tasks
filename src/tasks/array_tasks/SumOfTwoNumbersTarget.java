0175073f03e68cd7f5c45bd1fe1caf9a02cca18b
Hilal2013 <hilalozkul@gmail.com>
2023-01-20 11:58:40 -0500

    updated

--- src/tasks/array_tasks/SumOfTwoNumbersTarget.java
+++ src/tasks/array_tasks/SumOfTwoNumbersTarget.java
@@ -2,31 +2,34 @@ package tasks.array_tasks;
 
 import java.util.Arrays;
 
-public class SumOfTwoNumbersNine {
+public class SumOfTwoNumbersTarget {
     public static void main(String[] args) {
-        int[] arr = {1 , 2, 100, 7, 11, 15, 999 };
-
-        findSumOfTwoNumbersNine(arr);//[1, 3]
+        int[] arr = {1 , 2, 100, 7, 11, 8, 999 };
+int target=9;
+        findSumOfTwoNumbersTarget(arr,target);//[0, 5, 1, 3]
 
     }
 
-   public static void findSumOfTwoNumbersNine(int[] arr) {
+   public static void findSumOfTwoNumbersTarget(int[] arr,int target) {
        String result = "";
        for (int i = 0; i < arr.length; i++) {
-           for (int j = i; j < arr.length; j++) {
-               if (arr[i] + arr[j] == 9) {
+           for (int j = i+1; j < arr.length; j++) {
+               if (arr[i] + arr[j] == target) {
                    result += i + "" + j;
                }
            }
        }
        String[] str = result.split("");
-       System.out.println(Arrays.toString(str));//[1, 3]
+       System.out.println(Arrays.toString(str));//[0, 5, 1, 3]
+
        int size = str.length;//convert string array to int array
        int[] arr1 = new int[size];
        for (int i = 0; i < size; i++) {
            arr1[i] = Integer.parseInt(str[i]);
        }
        System.out.println(Arrays.toString(arr1));
+
+
    }
 }
 /*
