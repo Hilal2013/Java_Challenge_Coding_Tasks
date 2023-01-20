88df0cc66f2a39b57f53ef4c1692b54ab8acafba
Hilal2013 <hilalozkul@gmail.com>
2023-01-20 16:37:19 -0500

    updated

--- src/tasks/PhoneBook.java
+++ src/tasks/PhoneBook.java
@@ -6,30 +6,36 @@ import java.util.Scanner;
 public class PhoneBook {
     public static void main(String[] args) {
         // to get names and numbers
-        Scanner scan=new Scanner(System.in);//I want get them as I wanted //so create loop
-int last=scan.nextInt();
-        Map<String,Long> phoneBook = new java.util.HashMap<>();
-        for(int i = 0; i < last; i++){
-           scan.nextLine();
+        Scanner scan = new Scanner(System.in);//I want get them as I wanted //so create loop
+        int last = scan.nextInt();
+        Map<String, Long> phoneBook = new java.util.HashMap<>();
+        for (int i = 0; i < last; i++) {
+            scan.nextLine();
             String name = scan.nextLine();// to get names=> keys for map
             long phoneNumber = scan.nextLong();// to get numbers => values for map
-            phoneBook.put(name,phoneNumber);
-        }
-        System.out.println(phoneBook);
-        scan.nextLine();
+            if ((phoneNumber + "").length() == 8 && !(phoneNumber + "").substring(0, 1).equals("0")) {
+                phoneBook.put(name.toLowerCase(), phoneNumber);
+            } else {
+                System.out.println("Your phone number is not valid");
 
-        while(true){//it will continiue so we will use while loop
-            String name1=scan.nextLine();
-            if(phoneBook.containsKey(name1)){// if phonebook contains same name which is given from keybord
-                System.out.println(name1  + "=" +phoneBook.get(name1));
-            }else{
-                System.out.println("Not found");
             }
+
         }
+            System.out.println(phoneBook);
+            scan.nextLine();
+            while (true) {//it will continue so we will use while loop
+                String name1 = scan.nextLine();
+                if (phoneBook.containsKey(name1)) {// if phonebook contains same name which is given from keybord
+                    System.out.println(name1 + "=" + phoneBook.get(name1));
+                } else {
+                    System.out.println("Not found");
+                }
+            }
 
 //scan.close();?
 
-    }
+        }
+
 }
 /*
 scan.nextLine;
--- src/tasks/Practice.java
+++ src/tasks/Practice.java
@@ -5,11 +5,7 @@ import java.util.Arrays;
 
 public class Practice {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10,20,30,40));
-        list.add(1,30);
-        System.out.println(list);//[10, 30, 20, 30, 40]
-        list.remove(2);
-        System.out.println(list);//[10, 30, 30, 40]
+
 
 
     }
