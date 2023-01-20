053aac0d6ea46e2b42799d68bd33057e98668317
Hilal2013 <hilalozkul@gmail.com>
2023-01-20 08:19:47 -0500

    updated

--- src/tasks/InsertandDeleteList.java
+++ src/tasks/InsertandDeleteList.java
@@ -9,83 +9,48 @@ public class InsertandDeleteList {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int size= scan.nextInt();
-        int list1=scan.nextInt();
+      //  int list1=scan.nextInt();
         ArrayList<Integer> list = new ArrayList<>();
-       list.add(list1);
-       int index= scan.nextInt();
-        String op=scan.next();
-        if(op.equals("insert")){
-            if(list.size()<=index){
-                list.add(index);
-            }
+        for (int i = 0; i <size ; i++) {
+            int numbers=scan.nextInt();
+            list.add(numbers);
         }
-
-
-    }
-}
-/*
-if (op.equals("add")) {
-            if (list.size() <= (Integer.parseInt(id))) {
-                list.add(id + data);
-            } else {
-                for (int i = 0; i < list.size(); i++) {
-                    if ((list.get(i).substring(0, 1)).equals(id)) {
-                        list.add(Integer.parseInt(id) - 1, id + data);
-                        break;
+        System.out.println(list);
+       int turn=scan.nextInt();
+        for (int i = 0; i <turn ; i++) {
+            System.out.println("Please enter what do you want?");
+            String op=scan.next().toLowerCase();
+            if(op.equals("insert")){
+                System.out.println("to which index will you insert?");
+                int index= scan.nextInt();
+                System.out.println("What number do you want to insert?");
+                int insert=scan.nextInt();
+                if(list.size()<=index){
+                    list.add(insert);
+                }else{
+                    for (int j = 0; i < list.size(); j++) {
+                        if (list.get(j)==index){
+                            list.add( i- 1,insert);
+                            continue;
+                        }
                     }
                 }
             }
-            for (int i = 0; i < list.size(); i++) {
-                list.set(i, list.get(i).substring(1));
-            }
-            for (int i = 0, j = 1; i < list.size(); i++, j++) {
-                if (i == 0) {
-                    result += j + "" + list.get(i);
-                } else {
-                    result += "#" + j + list.get(i);
-                }
-            }
-            return result;
-        }
+            if(op.equals("Delete")){
+                System.out.println("which index will you delete?");
+                int index= scan.nextInt();
+                for (int j = 0; i < list.size(); j++) {
+                    if (list.get(j)==index){
+                        list.remove(index);
+                        continue;
+                    }
 
-        if (op.equals("edit")) {
-            list.set(Integer.parseInt(id)-1, id + data);
-            for (int i = 0; i < list.size(); i++) {
-                if (i == 0) {
-                    result += list.get(i);
-                } else {
-                    result += "#" + list.get(i);
-                }
-
-            }
-            return result;
-        }
-
-        if (op.equals("delete")) {
-            for (String each : list) {
-                if (("" + each.charAt(0)).equals(id)) {
-                    list.remove((Integer.parseInt(id)) - 1);
-                    break;
                 }
             }
         }
-        if ((Integer.parseInt(id)) == 1) {
-            for (int i = 0; i < list.size(); i++) {
-
-                result += "#" + list.get(i);
-            }
-        } else {
-            for (int i = 0; i < list.size(); i++) {
-                if (i == 0) {
-                    result += list.get(i);
-                } else {
-                    result += "#" + list.get(i);
-                }
-            }
-
-        }
-
-        return result;
-
+        System.out.println(list);
+    }
+}
+/*
 
  */
