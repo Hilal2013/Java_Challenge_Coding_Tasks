3119fac8d3bd5c835363a33659eb352a8cf3762f
Hilal2013 <hilalozkul@gmail.com>
2023-01-24 23:18:01 -0500

    updated

--- src/tasks/numbers_tasks/SwapTwoNumbers.java
+++ src/tasks/numbers_tasks/SwapTwoNumbers.java
@@ -4,34 +4,53 @@ import java.util.Scanner;
 
 public class SwapTwoNumbers {
     public static void main(String[] args) {
-
-        swapInteger(10, 20);//x=20 y=10
-swapString("Mary","John");//FirstString=John SecondString=Mary
-swapChar('A','B');//x=B y=A
+        swap(10, 20);////num1=20 num2=10
+        swapInteger(10, 20);//num1=20 num2=10
+        swapString("Mary", "John");//FirstString=John SecondString=Mary
+        swapChar('A', 'B');//x=B y=A
     }
-    public static void swapInteger(int x, int y) {
-        x += y;//x=x+y//x=10+20=30
-        y = x - y;//y=30-20=10
-        x -= y; //x=x-y//x=30-10=20
-        System.out.println("x=" + x + " y=" + y);
+
+    public static void swap(int num1, int num2) {//by declaring temporary value
+        int temp;
+        //first im gonna assign temp to num1
+        temp = num1; // if x is 10 temp will be 10
+        //second im gonna assign num2 to num1
+        num1 = num2;// num1 will be 20
+        //lastly im gonna assign temp to num2
+        num2 = temp;
+        System.out.println("num1=" + num1 + " num2=" + num2);
+
+
+    }
+
+    public static void swapInteger(int num1, int num2) {//by arithmetic  operator
+
+        //im gonna sum num1 and num2 and assign result to num1
+        num1 += num2;//num1=30
+        //substract num1 from num2 assign the result to num2
+        num2 = num1 - num2;//num2=30-20=10
+        //substract  num1 from  num2 assign the result to num1
+        num1 -= num2; //num1=30-10=20
+        System.out.println("num1=" + num1 + " num2=" + num2);
 
     }
 
     public static void swapString(String firstString, String secondString) {
-        int firstLength=firstString.length();
-    //    int secondlength=secondString.length();
-        firstString=firstString+""+secondString;//MaryJohn
-        secondString=firstString.substring(0,firstLength);//Mary
-        firstString=firstString.substring(firstLength);
+        int firstLength = firstString.length();
+        //    int secondlength=secondString.length();
+        firstString = firstString + "" + secondString;//MaryJohn
+        secondString = firstString.substring(0, firstLength);//Mary
+        firstString = firstString.substring(firstLength);
 
         System.out.println("FirstString=" + firstString + " SecondString=" + secondString);
 
     }
+
     public static void swapChar(char x, char y) {
 //A=65 B=66
-        x= (char) (x+y);
-        y=(char)(x-y);
-        x=(char)(x-y);
+        x = (char) (x + y);
+        y = (char) (x - y);
+        x = (char) (x - y);
 
         System.out.println("x=" + x + " y=" + y);
 
--- src/tasks/string_tasks/SortLettersAndNumbers.java
+++ src/tasks/string_tasks/SortLettersAndNumbers.java
@@ -1,6 +1,47 @@
 package tasks.string_tasks;
 
+import java.util.Arrays;
+
 public class SortLettersAndNumbers {
+    public static void main(String[] args) {
+
+        String str = "DC501GCCCA098911";
+        sortLettersAndNumbers(str);//CD015ACCCG01899
+
+    }
+
+    public static void sortLettersAndNumbers(String str) {
+
+        char[] arr = str.toCharArray();//to seperate charecters to use Character.isLetter etc..
+
+        String result = "";
+        for (int i = 0; i < arr.length - 1; i++) {//first imm gonna seperate letters and digits through comma
+            result += arr[i];
+            if (Character.isLetter(arr[i]) && Character.isDigit(arr[i + 1])) {
+                result += ",";//if there is digit after letter add comma
+
+            } else if (Character.isDigit(arr[i]) && Character.isLetter(arr[i + 1])) {
+
+                result += ",";////if there is letter after digit add comma
+            }
+        }
+        System.out.println(result);//DC,501,GCCCA,09891
+        //for sort im gonna create array through split
+        String[] arr1 = result.split(",");
+        System.out.println(Arrays.toString(arr1));//[DC, 501, GCCCA, 09891]
+        String resultSort = "";
+        for (int i = 0; i < arr1.length; i++) {//to access every index
+            for (int j = 0; j < arr1[i].length(); j++) {//to access element for each index
+                //to access each element in index and create new array for each index through split
+                String[] arr2 = arr1[i].split("");
+                Arrays.sort(arr2);//D,C will be C,D
+                resultSort += arr2[j];//CD
+            }
+        }
+        System.out.println(resultSort);//CD015ACCCG01899
+
+    }
+
 }
   /*
     Given alphanumeric String, we need to split the string into substrings of consecutive letters or numbers,
