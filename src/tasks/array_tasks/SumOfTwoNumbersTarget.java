bacbc45ebad22b8cfc54e6c1c09b15fe8d810e71
Hilal2013 <hilalozkul@gmail.com>
2023-02-12 21:01:36 -0500

    updated

--- src/tasks/array_tasks/SumOfTwoNumbersTarget.java
+++ src/tasks/array_tasks/SumOfTwoNumbersTarget.java
@@ -1,36 +1,59 @@
 package tasks.array_tasks;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SumOfTwoNumbersTarget {
     public static void main(String[] args) {
-        int[] arr = {1 , 2, 100, 7, 11, 8, 999 };
-int target=9;
-        findSumOfTwoNumbersTarget(arr,target);//[0, 5, 1, 3]
+        int[] nums = {2, 7, 11, 15};//[0,1]
+        int target = 9;
+//array[i]+array[j]=target value
+        //I can make nested loops and I can compare every item with another one//brute force solution
+        System.out.println(findSumOfTwoNumbersTarget(nums,target));//[0, 1]
+        System.out.println(findTwoSum(nums,target));//[0, 1]
 
     }
 
-   public static void findSumOfTwoNumbersTarget(int[] arr,int target) {
-       String result = "";
-       for (int i = 0; i < arr.length; i++) {
-           for (int j = i+1; j < arr.length; j++) {
-               if (arr[i] + arr[j] == target) {
-                   result += i + "" + j;
-               }
-           }
-       }
-       String[] str = result.split("");
-       System.out.println(Arrays.toString(str));//[0, 5, 1, 3]
+    public static String findSumOfTwoNumbersTarget(int[] nums, int target) {
 
-       int size = str.length;//convert string array to int array
-       int[] arr1 = new int[size];
-       for (int i = 0; i < size; i++) {
-           arr1[i] = Integer.parseInt(str[i]);
-       }
-       System.out.println(Arrays.toString(arr1));
+        int[] arr = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    //   arr = new int[]{i,j};
+                    arr[0]=i;
+                    arr[1]=j;
+                }
+            }
+        }
+        return Arrays.toString(arr);
+//2. What is the time complexity of your solution?
+        //nums.length=n    (c0+c1)*n*n remove constant-> time complexity=>  O(n^2)
 
+    }
+    //second solution
+    public static String findTwoSum(int[] nums, int target){
+        int[] arr = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        //im gonna create map key will be each element value will be index;
+        for (int i = 0; i < nums.length; i++) {//2 7 11 15
+            if (map.containsKey(target - nums[i])){//map contains element(target-element) dont add
+                arr[0] = map.get(target - nums[i]);//to get value means index
+                arr[1] = i;
 
-   }
+            }else{
+                map.put(nums[i], i);//add element
+            }
+
+        }
+        System.out.println(map);//{2=0, 11=2, 15=3}
+        return Arrays.toString(arr);
+//2. What is the time complexity of your solution?
+        //nums.length=n    (c0+c1+c2)*n remove constant-> time complexity=>  O(n)
+
+    }
+
 }
 /*
 input:numbers = {1 , 2, 100, 7, 11, 15, 999 };
