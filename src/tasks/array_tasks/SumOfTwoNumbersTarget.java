afe9eabbc7971b57cde97a574b1e3a9338a1fa05
Hilal2013 <hilalozkul@gmail.com>
2023-02-13 00:45:24 -0500

    updated

--- src/tasks/array_tasks/SumOfTwoNumbersTarget.java
+++ src/tasks/array_tasks/SumOfTwoNumbersTarget.java
@@ -8,51 +8,53 @@ public class SumOfTwoNumbersTarget {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};//[0,1]
         int target = 9;
-//array[i]+array[j]=target value
-        //I can make nested loops and I can compare every item with another one//brute force solution
-        System.out.println(findSumOfTwoNumbersTarget(nums,target));//[0, 1]
-        System.out.println(findTwoSum(nums,target));//[0, 1]
+        int[] result = findSumOfTwoNumbersTarget(nums, target);
+        System.out.println(Arrays.toString(result));//[0, 1]
+        int[] result2 = findTwoSum(nums, target);
+        System.out.println(Arrays.toString(result2));//[0, 1]
 
     }
 
-    public static String findSumOfTwoNumbersTarget(int[] nums, int target) {
+    public static int[] findSumOfTwoNumbersTarget(int[] nums, int target) {
 
         int[] arr = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    //   arr = new int[]{i,j};
-                    arr[0]=i;
-                    arr[1]=j;
+                    // arr[0]=i;  arr[1]=j;
+                    arr = new int[]{i, j};
+
                 }
             }
         }
-        return Arrays.toString(arr);
-//2. What is the time complexity of your solution?
+        return arr;
+//What is the time complexity of your solution?
         //nums.length=n    (c0+c1)*n*n remove constant-> time complexity=>  O(n^2)
-
     }
-    //second solution
-    public static String findTwoSum(int[] nums, int target){
+
+    //second solution for better time complexity
+    public static int[] findTwoSum(int[] nums, int target) {
+
         int[] arr = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
-        //im gonna create map key will be each element value will be index;
+        //im gonna create map key will be each element
+        // value will be index //to get index through using get()method
         for (int i = 0; i < nums.length; i++) {//2 7 11 15
-            if (map.containsKey(target - nums[i])){//map contains element(target-element) dont add
-                arr[0] = map.get(target - nums[i]);//to get value means index
+            if (map.containsKey(target - nums[i])) {//map contains the other element(target-element) //form your arr
+                arr[0] = map.get(target - nums[i]);//to get value means index//first element index
                 arr[1] = i;
 
-            }else{
-                map.put(nums[i], i);//add element
+            } else {
+                map.put(nums[i], i);//add element and index
             }
-
         }
         System.out.println(map);//{2=0, 11=2, 15=3}
-        return Arrays.toString(arr);
+        return arr;
 //2. What is the time complexity of your solution?
         //nums.length=n    (c0+c1+c2)*n remove constant-> time complexity=>  O(n)
 
     }
+
 
 }
 /*
