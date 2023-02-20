3f792b403baed63dbca031d42c44a9ec7aa138e0
Hilal2013 <hilalozkul@gmail.com>
2023-02-20 15:45:35 -0500

    updated sumUpTo0 task

--- src/tasks/array_tasks/SumUpTo0.java
+++ src/tasks/array_tasks/SumUpTo0.java
@@ -4,26 +4,53 @@ import java.util.Arrays;
 
 public class SumUpTo0 {
     public static void main(String[] args) {
-       int N=4; //for example 5 unique integers
-int[] result=findArraySumUpTo0(N);
+       int N=7; //for example=> it will print  4 unique numbers// N should be (1 < N < 100)
+        //we will use  symmetric pairs(-x,+x) to get sum=0 forexample (2,-2,5,-5)
+        //if N is odd we will add"0"
+findArraySumUpTo0(N);
+
+int[] result=sumZero(N);
         System.out.println(Arrays.toString(result));
     }
-    public static int[] findArraySumUpTo0(int N) {
-        int[] arr=new int[N];//new arrays's length will be=N
-        int sum=0;
-
-
-
-        for (int i = 0; i < arr.length; i++) {
-            sum+=arr[i];
-            if(sum==0){
-                return arr;
-            }
+    public static void findArraySumUpTo0(int N) {
+        for (int i = 1; i <= N/2 ; i++){
+            // if N is even
+            System.out.print(i + ", " + -i + ", ");
         }
 
+        // add extra 0 if N is odd
+        if (N % 2 == 1)
+            System.out.print(0);
+    }
+//solution two
+public static int[] sumZero(int N) {
+
+    int[] arr = new int[N];//array size will be N
+    int k=0;//starts array size with k
+    if(N==1){
+        arr[0]=0;
         return arr;
     }
-
+    if(N%2==0){//if N is even
+        for(int i=1;i<=N/2;i++){
+            arr[k]=i;
+            k++;
+            arr[k]=i*(-1);
+            k++;
+        }
+    }
+    else{//N is odd
+        arr[0]=0;
+        k=1;//first element 0 k willl start 1
+        for(int i=1;i<(N+1)/2;i++){
+            arr[k]=i;
+            k++;
+            arr[k]=i*(-1);
+            k++;
+        }
+    }
+    return arr;
+}
 
 
 }
