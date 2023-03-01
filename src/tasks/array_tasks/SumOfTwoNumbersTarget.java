104c6c72fdfb226596f1b34a4d81e01a6b550fe3
Hilal2013 <hilalozkul@gmail.com>
2023-02-28 19:52:15 -0500

    updated

--- src/tasks/array_tasks/SumOfTwoNumbersTarget.java
+++ src/tasks/array_tasks/SumOfTwoNumbersTarget.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class SumOfTwoNumbersTarget {
     public static void main(String[] args) {
-        int[] nums = {16, 3, 11,5,15};
+        int[] nums = {16, 3, 11, 5, 15};
         int target = 8;
         int[] result = findSumOfTwoNumbersTarget(nums, target);
         System.out.println(Arrays.toString(result));//[3, 5]
@@ -30,15 +30,30 @@ public class SumOfTwoNumbersTarget {
         return arr;
 //What is the time complexity  O(n^2)
     }
-
-    //second solution for better time complexity
+    //second solutionTwo point strategy;
     public static int[] findTwoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length-1;
+        int[] arr = new int[2];
+        while(left<right){
+            if(nums[left]+nums[right]==target){
+                arr = new int[]{nums[left], nums[right]};
+            }else if(nums[left]+nums[right]>target){
+                left++;
+
+            }else  right--;
+        }
+return arr;
+    }
+
+
+    public static int[] findTwoSum2(int[] nums, int target) {
 
         int[] arr = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
         //im gonna create map key will be each element
         // value will be index //to get index through using get()method
-        for (int i = 0; i < nums.length; i++) {//2 7 11 15
+        for (int i = 0; i < nums.length; i++) {//16, 3, 11,5,15
             if (map.containsKey(target - nums[i])) {//map contains the other element(target-element) //form your arr
                 arr[0] = map.get(target - nums[i]);//to get value means index//first element index
                 arr[1] = i;
